7053a4f5184753e21ed9eea0a9cd3aae3fc7af44
your name here <samuelchavez@students.berkeley.net>
2019-02-26 13:17:58 -0800

    cc

--- src/Runner.java
+++ src/Runner.java
@@ -1,3 +1,11 @@
 public class Runner {
-c
+    public static void main(String[] args) {
+        String q1 = "Where Is China?" + "(a)Asia\n(b)Europe\nSouth America\n ";
+
+        Question[] questions = {
+                new Question(q1, "a");
+
+    }
+
+    }
 }
